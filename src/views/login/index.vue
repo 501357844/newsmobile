c2329e77270fc90954f7c3e0f2f5a21eddd24861
501357844 <501357844@qq.com>
2019-07-29 14:24:45 +0800

    完成登陆布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,39 @@
 <template>
-  <div>login</div>
+  <div>
+    <van-nav-bar title="登陆" />
+    <form action>
+      <van-field
+        v-model="user.mobile"
+        required
+        label="手机号"
+        placeholder="请输入手机号"
+      />
+
+      <van-field v-model="user.code" type="password" label="密码" placeholder="请输入密码" required />
+    </form>
+    <van-button type="info" style="width:100%">登陆</van-button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.van-nav-bar {
+  background-color: #ccc;
+}
+.van-nav-bar__title {
+  color: #fff;
+}
 </style>
