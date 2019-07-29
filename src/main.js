9251af1cd57a24b564699652913d8ae85cec667a
501357844 <501357844@qq.com>
2019-07-29 13:45:37 +0800

    显示登陆页和首页 以及前期的准备工作

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import Vant from 'vant'
 import router from './router'
-import store from './store'
+import 'amfe-flexible/index.min.js'
+import 'vant/lib/index.css'
+// import store from './store'
+
+Vue.use(Vuex)
+Vue.use(Vant)
+const store = new Vuex.Store({
+  // 声明数据
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  }
+
+})
 
 Vue.config.productionTip = false
 
